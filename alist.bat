614fa995214026846a5961357e58c7670bae6012
Lmoe <140913032+CN-Lmoe@users.noreply.github.com>
2025-06-05 19:05:55 +0800

    Add files via upload

---
+++ alist.bat
@@ -0,0 +1,3 @@
+@echo off
+start C:\Tools\alist-windows-amd64\alist.exe server
+start http://localhost:5244
---
+++ aria2.bat
@@ -0,0 +1,2 @@
+@echo off
+start C:\Tools\aria2-1.37.0-win-64bit-build1\aria2c.exe --enable-rpc --rpc-listen-all
